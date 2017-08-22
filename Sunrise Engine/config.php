f146cdeacc4b4a834eda18f8be8e38cc54359714
Alexander Gluschenko <qugluschenko@gmail.com>
2017-08-22 19:31:55 +0300

    Security Update 2

--- Sunrise Engine/config.php
+++ Sunrise Engine/config.php
@@ -14,9 +14,9 @@ $config['mysql_user'] = "user"; //Палево
 $config['mysql_password'] = "password"; //Палево
 $config['mysql_name'] = "database"; //Палево
 
-$config['salt'] = "hgfjdgjd"; //Палево
-$config['sig_salt'] = "vbmvbmh"; //Палево
-$config['admin_salt'] = "sfdsbbtfv"; //Палево
+$config['salt'] = "123"; //Палево
+$config['sig_salt'] = "456"; //Палево
+$config['admin_salt'] = "789"; //Палево
 
 $config['admin_email'] = "user@gmail.com"; // Email администратора (ОБЯЗАТЕЛЬНО ИЗМЕНИТЬ НА СВОЙ)
 
